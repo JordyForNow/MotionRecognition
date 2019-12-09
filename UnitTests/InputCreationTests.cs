a2202acf2a56935d6d6395f83d60e3495425fff3
Felix <felixbeuving@live.nl>
2019-12-09 10:21:53 +0100

    Add UnitTest project

---
+++ UnitTests/InputCreationTests.cs
@@ -0,0 +1,20 @@
+﻿using MotionRecognition;
+using NUnit.Framework;
+using System.IO;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestLoader()
+        {
+			CSVLoader ldr = new CSVLoader(Directory.GetCurrentDirectory() + "data.csv");
+        }
+    }
+}
