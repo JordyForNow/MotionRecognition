a0a16851d977ae33f45242d98ed9839a7da262db
JordyForNow <s1114360@student.windesheim.nl>
2019-12-16 12:17:36 +0100

    unit test fix, added os check

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using MotionRecognition;
 
 namespace Sandbox
@@ -7,14 +9,31 @@ namespace Sandbox
 	{
 		static void Main(string[] args)
 		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				Console.WriteLine("Windows");
+			} else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				Console.WriteLine("Linux");
+			} else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				Console.WriteLine("OSX");
+			}
+
+			//string path = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+			//if (Environment.OSVersion.Version.Major >= 6)
+			//{
+			//	path = Directory.GetParent(path).ToString();
+			//}
+			//Console.WriteLine(path);
 			// TODO: make relative
 			Environment.SetEnvironmentVariable("PATH", @"C:\Users\Jordy\AppData\Local\Programs\Python\Python36", EnvironmentVariableTarget.Process);
 			Environment.SetEnvironmentVariable("PYTHONHOME", @"C:\Users\Jordy\AppData\Local\Programs\Python\Python36\python.exe", EnvironmentVariableTarget.Process);
 			NetworkController control = new NetworkController();
-			uint[,,] d = new uint[1,1,1];
-			bool[] b = new bool[1];
-			control.TrainNetwork(ref d, b);
-			control.Run();
+			//uint[,,] d = new uint[1,1,1];
+			//bool[] b = new bool[1];
+			//control.TrainNetwork(ref d, b);
+			
 		}
 	}
 }
