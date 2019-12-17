ed7914531b852effa964641e725ec9ae9f1e5c49
jetenergy <renegrapjas@hotmail.com>
2019-12-17 13:00:55 +0100

    folder structure refactor

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,12 +6,13 @@ namespace Sandbox
 	{
 		static void Main(string[] args)
 		{
-			CSVLoader loader = new CSVLoader("data.csv", 21);
+			IDataLoader<JointMeasurement> loader = new CSVLoader("data.csv", 21);
 
 			var table = loader.GetData();
 
 			Motion3DImage image = new Motion3DImage(100);
 			image.CreateImageFromTable(ref table);
+
 			ImageSerializer.Serialize(image);
 			Motion3DImage image2 = ImageSerializer.DeSerialize();
 			ImageSerializer.Serialize(image2, "./data2");
