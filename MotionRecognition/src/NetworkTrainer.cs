564bc69a16a9edb5aff206caa92c90ba70ee02ce
JordyForNow <s1114360@student.windesheim.nl>
2019-12-18 12:33:37 +0100

    rewrote trainer

--- MotionRecognition/src/NetworkTrainer.cs
+++ MotionRecognition/src/NetworkTrainer.cs
@@ -44,74 +44,67 @@ namespace MotionRecognition
 
 		public bool Run()
 		{
-
-			// input image dimensions
-			int img_rows = inputSize * 2, img_cols = inputSize;
-			int num_classes = 2;
-
-			Shape input_shape = null;
-
-			// the data, split between train and test sets
-			var x_train = trainingData;
-			var y_train = trainingAnswers;
-			//Console.WriteLine(x_train);
-
-			if (Backend.ImageDataFormat() == "channels_first")
+			try
 			{
-				x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols);
-				//x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols);
-				input_shape = (1, img_rows, img_cols);
+				// Input image dimensions.
+				int imgRows = inputSize * 2;
+				int imgCols = inputSize;
+				int numClasses = 2;
+
+				Shape input_shape = null;
+
+				if (Backend.ImageDataFormat() == "channels_first")
+				{
+					trainingData = trainingData.reshape(trainingData.shape[0], 1, imgRows, imgCols);
+					input_shape = (1, imgRows, imgCols);
+				}
+				else
+				{
+					trainingData = trainingData.reshape(trainingData.shape[0], imgRows, imgCols, 1);
+					input_shape = (imgRows, imgCols, 1);
+				}
+
+				trainingData = trainingData.astype(np.float32);
+				trainingData /= 255;
+				Console.WriteLine($"trainingData shape: {trainingData.shape}");
+				Console.WriteLine($"{trainingData.shape[0]} train samples");
+
+				// Convert class vectors to binary class matrices.
+				trainingAnswers = Util.ToCategorical(trainingAnswers, numClasses);
+
+				// Build CNN model.
+				var model = new Sequential();
+				model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
+										activation: "relu",
+										input_shape: input_shape));
+				model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
+				model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
+				model.Add(new Dropout(0.25));
+				model.Add(new Flatten());
+				model.Add(new Dense(128, activation: "relu"));
+				model.Add(new Dropout(0.5));
+				model.Add(new Dense(numClasses, activation: "softmax"));
+
+				model.Compile(loss: "categorical_crossentropy",
+					optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
+
+				model.Fit(trainingData, trainingAnswers,
+							batch_size: batchSize,
+							epochs: epochs,
+							verbose: 1);
+
+				// Save model and weights.
+				string json = model.ToJson();
+				File.WriteAllText(outputDirectory + outputName + ".json", json);
+				model.SaveWeight(outputDirectory + outputName + ".h5");
 			}
-			else
+			catch (Exception e)
 			{
-				x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1);
-				//x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1);
-				input_shape = (img_rows, img_cols, 1);
+				Console.WriteLine(e);
+				return false;
 			}
 
-			x_train = x_train.astype(np.float32);
-			//x_test = x_test.astype(np.float32);
-			x_train /= 255;
-			//x_test /= 255;
-			Console.WriteLine($"x_train shape: {x_train.shape}");
-			Console.WriteLine($"{x_train.shape[0]} train samples");
-			//Console.WriteLine($"{x_test.shape[0]} test samples");
-
-			// convert class vectors to binary class matrices
-			y_train = Util.ToCategorical(y_train, num_classes);
-			//y_test = Util.ToCategorical(y_test, num_classes);
-
-			// Build CNN model
-			var model = new Sequential();
-			model.Add(new Conv2D(32, kernel_size: (3, 3).ToTuple(),
-									activation: "relu",
-									input_shape: input_shape));
-			model.Add(new Conv2D(64, (3, 3).ToTuple(), activation: "relu"));
-			model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
-			model.Add(new Dropout(0.25));
-			model.Add(new Flatten());
-			model.Add(new Dense(128, activation: "relu"));
-			model.Add(new Dropout(0.5));
-			model.Add(new Dense(num_classes, activation: "softmax"));
-
-			model.Compile(loss: "categorical_crossentropy",
-				optimizer: new Adadelta(), metrics: new string[] { "accuracy" });
-
-			model.Fit(x_train, y_train,
-						batch_size: batchSize,
-						epochs: epochs,
-						verbose: 1);
-			//var score = model.Evaluate(x_test, y_test, verbose: 0);
-			//Console.WriteLine($"Test loss: {score[0]}");
-			//Console.WriteLine($"Test accuracy: {score[1]}");
-
-			//Save model and weights
-			string json = model.ToJson();
-			File.WriteAllText(outputDirectory + outputName + ".json", json);
-			model.SaveWeight(outputDirectory + outputName + ".h5");
-
-
-			return false;
+			return true;
 		}
 
 	}
