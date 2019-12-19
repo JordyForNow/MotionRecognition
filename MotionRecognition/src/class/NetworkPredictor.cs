54d22d720f79f5c614922eb37f9ff99e40874380
JordyForNow <s1114360@student.windesheim.nl>
2019-12-19 14:08:41 +0100

    added classes

---
+++ MotionRecognition/src/class/NetworkPredictor.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace MotionRecognition
+{
+	class NetworkPredictor
+	{
+		private int networkInputSize;
+
+		public NetworkPredictor(string _networkWeights,
+								string _networkLayers,
+								int[,] _inputData,
+								int _networkInputSize)
+		{
+			//Load model and weight
+			networkInputSize = _networkInputSize;
+		}
+
+		public bool Run()
+		{
+			return false;
+		}
+
+	}
+}
---
+++ MotionRecognition/src/class/NetworkTrainer.cs
@@ -0,0 +1,33 @@
+﻿namespace MotionRecognition
+{
+	class NetworkTrainer
+	{
+		private string outputDirectory;
+		private string outputName;
+		private int inputSize;
+		private int epochs;
+		private int batchSize;
+
+		public NetworkTrainer(
+			ref int[,,] _inputData,
+			ref int[] _inputAnswers,
+			string _outputDirectory,
+			string _outputName,
+			int _inputSize,
+			int _epochs,
+			int _batchSize)
+		{
+			outputDirectory = _outputDirectory;
+			outputName = _outputName;
+			inputSize = _inputSize;
+			epochs = _epochs;
+			batchSize = _batchSize;
+		}
+
+		public bool Run()
+		{
+			return false;
+		}
+
+	}
+}
