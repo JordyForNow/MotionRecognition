2eebaa6b87e3bb61efe9f94f9d0b7a11ff6e29be
JordyForNow <s1114360@student.windesheim.nl>
2019-12-19 13:25:20 +0100

    fix

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,14 +6,7 @@ namespace Sandbox
 	{
 		static void Main(string[] args)
 		{
-			CSVLoader loader = new CSVLoader("data.csv", 21);
-
-			var table = loader.GetData();
-
-			Motion3DImage image = new Motion3DImage(ref table);
-			ImageSerializer.Serialize(image);
-			Motion3DImage image2 = ImageSerializer.DeSerialize();
-			ImageSerializer.Serialize(image2, "./data2");
+			
 		}
 	}
 }
