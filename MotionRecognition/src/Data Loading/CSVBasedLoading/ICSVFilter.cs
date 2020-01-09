7794113a1a59ec527186b32551fee43001c14ee0
Velikss <felixbeuving@live.nl>
2020-01-09 15:01:14 +0100

    added interface that got removed

--- MotionRecognition/src/Data Loading/CSVBasedLoading/ICSVFilter.cs
+++ MotionRecognition/src/Data Loading/CSVBasedLoading/ICSVFilter.cs
@@ -1,6 +1,6 @@
 ﻿namespace MotionRecognition
 {
-	public interface ICSVFilter
+	public interface ICSVFilter : IDataFilter
 	{
 		// True means that the column needs to be interpreted.
 		bool Use(ref string[] row, uint index);
