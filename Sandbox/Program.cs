0d3b62c7118ab4127714e269bef22d2a4bf3c840
jetenergy <renegrapjas@hotmail.com>
2020-01-14 12:48:37 +0100

    Changed InputManipulation to DLL

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,5 @@
-﻿using MotionRecognition;
+﻿using InputManipulation;
+using MotionRecognition;
 using System;
 using System.Collections.Generic;
 
@@ -36,7 +37,62 @@ namespace Sandbox
 			var arr = transformer.GetNeuralInput(transformerSettings);
 			foreach (var d in arr)
 				Console.WriteLine(d);
+		}
 
+		static void ManipulateAndImageCreate()
+		{
+			#region Csv Manipulation
+			CSVManipulatorSettings manipulatorSettings = new CSVManipulatorSettings // Create manipulation settings
+			{
+				copyLines = 1,
+				removeLines = 1,
+				mutationCount = 5,
+				deviationPercentage = 0.05f,
+				innerDeviationPercentage = 0.01f,
+				dataFile = "",
+				dataFolder = "./CSV/",
+				outputFolder = "./mutated/",
+				alterInput = true
+			};
+
+			CSVManipulator.RunManipulator(ref manipulatorSettings);
+			#endregion
+
+			#region Image creation related calls
+			System.IO.FileInfo[] FileInfo = new System.IO.DirectoryInfo(manipulatorSettings.outputFolder).GetFiles();
+
+			CSVLoaderSettings csvLoaderSettings = new CSVLoaderSettings
+			{
+				CSVHasHeader = true,
+				trimUp = 0,
+				trimDown = 0
+			};
+
+			List<ICSVFilter> csvFilters = new List<ICSVFilter>(1);
+
+			ICSVFilter evenFilter = new CSVEvenColumnFilter();
+			csvFilters.Add(evenFilter);
+			csvLoaderSettings.filters = csvFilters;
+
+			ImageTransformerSettings transformerSettings = new ImageTransformerSettings
+			{
+				focusJoints = (LeapMotionJoint[])Enum.GetValues(typeof(LeapMotionJoint)),
+				size = 100
+			};
+
+			foreach (var file in FileInfo)
+			{
+				csvLoaderSettings.filePath = file.FullName;
+
+				var csvData = CSVLoader<Vector3>.LoadData(ref csvLoaderSettings);
+
+				transformerSettings.samples = csvData;
+
+				var outputArr = new ImageTransformer().GetNeuralInput(transformerSettings);
+
+				ImageCreator.WriteBitmapToFS(ImageCreator.CreateNeuralImageFromDoubleArray(ref outputArr, transformerSettings.size, true), "./IMG/" + file.Name);
+			}
+			#endregion
 		}
 	}
 }
