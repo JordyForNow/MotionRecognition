a8245d25ca337c12caee7981c92d8c6bedc15a0b
JordyForNow <s1114360@student.windesheim.nl>
2020-01-14 14:23:37 +0100

    reverted changes, moved files to folder

---
+++ MotionRecognition/src/RecognizerController/INetworkPredictController.cs
@@ -0,0 +1,11 @@
+﻿namespace MotionRecognition
+{
+	public interface INetworkPredictController<T>
+	{
+
+		static void PreparePredictor(ref NetworkContainer container, ref T settings) { }
+		
+		static bool Predict(ref NetworkContainer container, ref T settings) { return false; }
+
+	}
+}
