806651e4aabd2577a3595be6d19eff79bcc3c149
JordyForNow <s1114360@student.windesheim.nl>
2020-01-14 11:23:07 +0100

    debug to rene

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,48 +1,4 @@
-﻿//using System;
-//using System.IO;
-//using MotionRecognition;
-
-//namespace Sandbox
-//{
-//	class Program
-//	{
-//		static void Main(string[] args)
-//		{
-//			NetworkContainer container = new NetworkContainer
-//			{
-//				verbose = true
-//			};
-
-//			ImageNetworkTrainSettings trainSettings = new ImageNetworkTrainSettings
-//			{
-//				correctInputDirectory = @"../../../CorrectTestData",
-//				incorrectInputDirectory = @"../../../IncorrectTestData",
-
-//				outputDirectory = @"../../../DataOut",
-//				outputName = "ModelOne",
-
-//				sampleCount = 10
-//			};
-
-//			ImageNetworkTrainController.prepareData(ref trainSettings, ref container);
-//			ImageNetworkTrainController.prepareNetwork(ref trainSettings, ref container);
-//			ImageNetworkTrainController.train(ref trainSettings, ref container);
-
-//			ImageNetworkPredictSettings predictSettings = new ImageNetworkPredictSettings
-//			{
-//				trainedNetwork = @"../../../DataOut\ModelOne.eg",
-//				predictData = @"../../../testCor\0-2.csv",
-
-//				networkInputSize = 10
-//			};
-
-//			ImageNetworkPredictController.preparePredictor(ref predictSettings, ref container);
-//			Console.WriteLine(ImageNetworkPredictController.predict(ref predictSettings, ref container));
-//		}
-//	}
-//}
-
-using System;
+﻿using System;
 using System.IO;
 using MotionRecognition;
 
@@ -57,7 +13,7 @@ namespace Sandbox
 				verbose = true
 			};
 
-			CountNetworkTrainSettings trainSettings = new CountNetworkTrainSettings
+			ImageNetworkTrainSettings trainSettings = new ImageNetworkTrainSettings
 			{
 				correctInputDirectory = @"../../../CorrectTestData",
 				incorrectInputDirectory = @"../../../IncorrectTestData",
@@ -68,21 +24,65 @@ namespace Sandbox
 				sampleCount = 10
 			};
 
-			CountNetworkTrainController.prepareData(ref trainSettings, ref container);
-			CountNetworkTrainController.prepareNetwork(ref trainSettings, ref container);
-			CountNetworkTrainController.train(ref trainSettings, ref container);
+			ImageNetworkTrainController.prepareData(ref trainSettings, ref container);
+			ImageNetworkTrainController.prepareNetwork(ref trainSettings, ref container);
+			ImageNetworkTrainController.train(ref trainSettings, ref container);
 
-			CountNetworkPredictSettings predictSettings = new CountNetworkPredictSettings
+			ImageNetworkPredictSettings predictSettings = new ImageNetworkPredictSettings
 			{
-				trainedNetwork = @"../../../DataOut/ModelOne.eg",
-				predictData = @"../../../TestCor/0-2.csv",
+				trainedNetwork = @"../../../DataOut\ModelOne.eg",
+				predictData = @"../../../testCor\0-2.csv",
 
 				networkInputSize = 10
 			};
 
-			CountNetworkPredictController.preparePredictor(ref predictSettings, ref container);
-			Console.WriteLine(CountNetworkPredictController.predict(ref predictSettings, ref container));
+			ImageNetworkPredictController.preparePredictor(ref predictSettings, ref container);
+			Console.WriteLine(ImageNetworkPredictController.predict(ref predictSettings, ref container));
 		}
 	}
 }
+
+//using System;
+//using System.IO;
+//using MotionRecognition;
+
+//namespace Sandbox
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			NetworkContainer container = new NetworkContainer
+//			{
+//				verbose = true
+//			};
+
+//			CountNetworkTrainSettings trainSettings = new CountNetworkTrainSettings
+//			{
+//				correctInputDirectory = @"../../../CorrectTestData",
+//				incorrectInputDirectory = @"../../../IncorrectTestData",
+
+//				outputDirectory = @"../../../DataOut",
+//				outputName = "ModelOne",
+
+//				sampleCount = 10
+//			};
+
+//			CountNetworkTrainController.prepareData(ref trainSettings, ref container);
+//			CountNetworkTrainController.prepareNetwork(ref trainSettings, ref container);
+//			CountNetworkTrainController.train(ref trainSettings, ref container);
+
+//			CountNetworkPredictSettings predictSettings = new CountNetworkPredictSettings
+//			{
+//				trainedNetwork = @"../../../DataOut/ModelOne.eg",
+//				predictData = @"../../../TestCor/0-2.csv",
+
+//				networkInputSize = 10
+//			};
+
+//			CountNetworkPredictController.preparePredictor(ref predictSettings, ref container);
+//			Console.WriteLine(CountNetworkPredictController.predict(ref predictSettings, ref container));
+//		}
+//	}
+//}
 
